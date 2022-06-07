8aff193bbb653db05732ba7a0f19bdec7e0b79d9
Konstantinos Drosos <kostas.drosos@icloud.com>
2022-06-07 13:49:57 +0300

    Now using python script for the weather info

---
+++ get_weather.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""Script to get the weather."""
+
+__docformat__ = 'reStructuredText'
+
+from subprocess import run, PIPE
+
+
+def main():
+    weather = run(
+        'curl wttr.in/?format="%c+%t"',
+        shell=True,
+        stdout=PIPE
+    ).stdout.rstrip().decode('utf-8').split()
+
+    if len(weather) == []:
+        print('No weather info', flush=True, end='')
+    else:
+        print(f'{weather[0]}  {weather[1]}', flush=True, end='')
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
